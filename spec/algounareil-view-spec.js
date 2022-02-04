e5abb331d3ade18646ea8e3d84db39fd34798474
unareil <contact@unareil.eu>
2022-02-04 14:45:54 +0100

    remise au propre du depot pour garder uniquement l'essentiel

--- spec/algounareil-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AlgounareilView from '../lib/algounareil-view';
-
-describe('AlgounareilView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
